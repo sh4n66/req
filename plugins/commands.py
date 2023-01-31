65587f8bd90faa706de3fbaeae3305ee15c5ec54
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2023-01-31 20:49:09 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -55,7 +55,7 @@ async def start(client, message):
         ]]         
         reply_markup = InlineKeyboardMarkup(buttons)
         m=await message.reply_sticker("CAACAgUAAxkBAAEOYa5j2O_PzQ2G7X6h8WRgPO-cEUSu_QAC6wUAAocZUFUHLJQ6qdlyfB4E") 
-        await asyncio.sleep(1)
+        await asyncio.sleep(3)
         await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
