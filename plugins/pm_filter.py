287870d2f3b4b67ef8dab80b699155ce25ef732a
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2023-02-17 15:42:23 +0600

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1306,7 +1306,8 @@ async def advantage_spell_chok(client, msg):
         for k, movie_name in enumerate(movielist)
     ]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spol#{reqstr1}#close_spellcheck')])
-    spell_check_del = await msg.reply_text(
+    spell_check_del = await msg.reply_photo(
+        photo=(SPELL_IMG),
        
         caption=(script.CUDNT_FND.format(reqstr.mention)),
         reply_markup=InlineKeyboardMarkup(btn)
