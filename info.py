675c33fe743db0056cd3bafec4bd88a14cd8e11d
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2023-02-17 13:59:50 +0600

    Update info.py

--- info.py
+++ info.py
@@ -50,8 +50,8 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '6154665809:AAHFZB2IR4a6Qkuqf5JVMS1fztK9OjE
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS', 'https://telegra.ph/file/25256796dbd8333d17fa8.jpg')).split()
-NOR_IMG = environ.get('NOR_IMG', "https://telegra.ph/file/25256796dbd8333d17fa8.jpg")
-SPELL_IMG = environ.get('SPELL_IMG',"https://telegra.ph/file/25256796dbd8333d17fa8.jpg")
+NOR_IMG = environ.get('NOR_IMG', "None")
+SPELL_IMG = environ.get('SPELL_IMG',"None")
 
 # Welcome area
 MELCOW_IMG = environ.get('MELCOW_IMG',"https://telegra.ph/file/25256796dbd8333d17fa8.jpg")
