f1a0e68e0423d3f316af94e78cbd5e773b04b7be
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2023-03-16 13:48:38 +0600

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -135,7 +135,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"🎭[CT™]{get_size(file.file_size)} ⇛ {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"🎭[CT™]{get_size(file.file_size)} ➽ {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
@@ -1127,7 +1127,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"▫️{get_size(file.file_size)} ⊳ {file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"🎭[CT™]{get_size(file.file_size)} ➽ {file.file_name}", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
