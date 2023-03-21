b5c1d5c058db2bc97b9915bdeddd1b699f93115e
hemanthss2004 <96716772+hemanthss2004@users.noreply.github.com>
2023-03-21 07:54:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/hemanthss2004/req-repo.git /Elsa
+  git clone https://github.com/hemanthss2004/req-repo/tree/hemanthss2004-patch-1 /Elsa
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Elsa
