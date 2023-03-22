6d7a7a0dd5aa8635578744281019f1174597c1c9
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2023-03-22 12:27:36 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/sathanxavier1998/req-repo.git /CTAutofilter
+  git clone https://github.com/sathanxavier1998/req-repo.git /Elsa
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /CTAutofilter
+  git clone $UPSTREAM_REPO /Elsa
 fi
-cd /CTAutofilter
+cd /Elsa
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝘾𝙏 𝘼𝙪𝙩𝙤𝙛𝙞𝙡𝙩𝙚𝙧...🧞‍♂️"
 python3 bot.py
