586d4dac7397eb90a2557222cec1cf4aa5ad1a23
kimsohyun385@gmail.com <kimsohyun385@gmail.com>
2023-10-16 12:08:50 +0530

    broadcast code fixed

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,73 +1,79 @@
 from pyrogram import Client, filters
-import datetime
-import time
 from database.users_chats_db import db
 from info import ADMINS
-from utils import broadcast_messages
-import asyncio
+import asyncio, datetime, time
         
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
-# https://t.me/GetTGLink/4178
-async def verupikkals(bot, message):
+async def broadcast_function(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
-    sts = await message.reply_text(
-        text='Broadcasting your messages...'
-    )
-    start_time = time.time()
+    sts = await message.reply_text("Broadcasting your messages...")
     total_users = await db.total_users_count()
     done = 0
     blocked = 0
     deleted = 0
     failed =0
-
     success = 0
+    start_time = time.time()
     async for user in users:
-        pti, sh = await broadcast_messages(int(user['id']), b_msg)
-        if pti:
+        user_id = int(user['id'])
+        try:
+            await b_msg.copy(chat_id=user_id)
             success += 1
-        elif pti == False:
-            if sh == "Blocked":
-                blocked+=1
-            elif sh == "Deleted":
-                deleted += 1
-            elif sh == "Error":
-                failed += 1
+        except FloodWait as e:
+            await asyncio.sleep(e.value)
+            await b_msg.copy(chat_id=user_id)
+        except InputUserDeactivated:
+            await db.delete_user(user_id)
+            logging.info(f"{user_id} - Removed from Database, since deleted account.")
+            deleted += 1
+        except UserIsBlocked:
+            logging.info(f"{user_id} -Blocked the bot.")
+            blocked+=1
+        except PeerIdInvalid:
+            await db.delete_user(int(user_id))
+            logging.info(f"{user_id} - PeerIdInvalid")
+            failed += 1
+        except Exception as e:
+            failed += 1
         done += 1
-        await asyncio.sleep(2)
-        if not done % 20:
+        if not done % 100:
             await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+    await sts.delete()
+    await message.reply_text(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}", quote=True)
 
 
 @Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS) & filters.reply)
-async def grp_brodcst(bot, message):
+async def group_broadcast(bot, message):
     chats = await db.get_all_chats()
     b_msg = message.reply_to_message
-    sts = await message.reply_text(
-        text='Broadcasting your messages...'
-    )
-    start_time = time.time()
+    sts = await message.reply_text('Broadcasting your messages...')
     total_chats = await db.total_chat_count()
     done = 0
     failed =0
-
     success = 0
+    start_time = time.time()
     async for chat in chats:
-        pti, sh = await broadcast_messages(int(chat['id']), b_msg)
-        if pti:
+        chat_id = int(chat['id'])
+        try:
+            await b_msg.copy(chat_id)
             success += 1
-        elif pti == False:
-            if sh == "Blocked":
-                blocked+=1
-            elif sh == "Deleted":
-                deleted += 1
-            elif sh == "Error":
-                failed += 1
+        except FloodWait as e:
+            await asyncio.sleep(e.value)
+            await b_msg.copy(chat_id)
+        except PeerIdInvalid:
+            await db.delete_user(int(user_id))
+            logging.info(f"{chat_id} - PeerIdInvalid")
+            failed += 1
+        except Exception as e:
+            failed += 1
         done += 1
-        await asyncio.sleep(2)
-        if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Chats {total_chats}\nCompleted: {done} / {total_chats}\nSuccess: {success}\nFailed: {failed}")    
+        if not done % 100:
+            await sts.edit(f"Broadcast in progress:\n\nTotal Chats {total_users}\nCompleted: {done}/{total_chats}\nSuccess: {success}\nFailed: {failed}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Chats {total_chats}\nCompleted: {done} / {total_chats}\nSuccess: {success}\nFailed: {failed}")
+    await sts.delete()
+    await message.reply_text(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Chats {total_chats}\nCompleted: {done} / {total_users}\nSuccess: {success}\nFailed: {failed}", quote=True)
+
+
+
--- utils.py
+++ utils.py
@@ -148,28 +148,6 @@ async def get_poster(query, bulk=False, id=False, file=None):
         'rating': str(movie.get("rating")),
         'url':f'https://www.imdb.com/title/tt{movieid}'
     }
-# https://github.com/odysseusmax/animated-lamp/blob/2ef4730eb2b5f0596ed6d03e7b05243d93e3415b/bot/utils/broadcast.py#L37
-
-async def broadcast_messages(user_id, message):
-    try:
-        await message.copy(chat_id=user_id)
-        return True, "Success"
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        return await broadcast_messages(user_id, message)
-    except InputUserDeactivated:
-        await db.delete_user(int(user_id))
-        logging.info(f"{user_id}-Removed from Database, since deleted account.")
-        return False, "Deleted"
-    except UserIsBlocked:
-        logging.info(f"{user_id} -Blocked the bot.")
-        return False, "Blocked"
-    except PeerIdInvalid:
-        await db.delete_user(int(user_id))
-        logging.info(f"{user_id} - PeerIdInvalid")
-        return False, "Error"
-    except Exception as e:
-        return False, "Error"
 
 async def search_gagala(text):
     usr_agent = {
