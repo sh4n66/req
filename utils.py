23f67bd0bb4adffe59ccad142c88fc8eeefd905c
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2023-10-20 22:02:06 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -71,7 +71,7 @@ async def is_subscribed(bot, query):
             return False
 
 async def get_poster(query, bulk=False, id=False, file=None):
-if not id:
+    if not id:
         # https://t.me/GetTGLink/4183
         query = (query.strip()).lower()
         title = query
