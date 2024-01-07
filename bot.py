755db3c097e3b157f09eddd2ecb463a169858b73
AfsalHashim <138692053+CyberTG@users.noreply.github.com>
2024-01-07 21:06:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
 from database.join_reqs import JoinReqs
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL, PORT
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL, PORT, REQ_CHANNEL
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
