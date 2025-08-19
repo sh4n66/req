bb0ad70afb192dc3d0ea1f0742971264eda38c1b
sh4n66 <boteusefor6@gmail.com>
2025-08-19 17:35:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -76,10 +76,10 @@ TMP_DOWNLOAD_DIRECTORY = environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://favouratetechy:Sh4n66@6sha@sh4nteam.kvctgbj.mongodb.net/?retryWrites=true&w=majority&appName=Sh4nteam")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Sh4nteam")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://cleancoreai33:zmFReEgYg1zWQVVG@cluster0.wbpexbe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
-MONGO_URL = os.environ.get('MONGO_URL', "mongodb+srv://favouratetechy:Sh4n66@6sha@sh4nteam.kvctgbj.mongodb.net/?retryWrites=true&w=majority&appName=Sh4nteam")
+MONGO_URL = os.environ.get('MONGO_URL', "mongodb+srv://cleancoreai33:zmFReEgYg1zWQVVG@cluster0.wbpexbe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 #Downloader
 DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/AudioBoT/")
